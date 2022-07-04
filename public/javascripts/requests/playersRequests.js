460f2a916971a806c2a904ee89d06207f444c17d
pmbnarra <108690403+pmbnarra@users.noreply.github.com>
2022-07-05 00:28:41 +0100

    Update playersRequests.js

--- public/javascripts/requests/playersRequests.js
+++ public/javascripts/requests/playersRequests.js
@@ -75,9 +75,7 @@ async function register(name, password) {
             body: JSON.stringify({ name: name, password: password}) 
         });
         var result = await response.json();
-        // We are not checking for errors (considering the GUI is only allowing correct choices)
         return {logged: response.status==200 , result: result };
-        return result;
     } catch (err) {
         // Treat 500 errors here
         console.log(err);
