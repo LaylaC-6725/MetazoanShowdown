119545b448021dffe4b8f71d09bff02e1d2521ae
pmbnarra <108690403+pmbnarra@users.noreply.github.com>
2022-07-04 22:50:01 +0100

    Update login.js

--- public/javascripts/authentication/login.js
+++ public/javascripts/authentication/login.js
@@ -2,7 +2,10 @@ async function loginPlayer() {
     try {
         let name = document.getElementById("name").value;
         let password = document.getElementById("password").value;
-        let result = await login(name, password);
+        
+        var encryptPass = CryptoJS.SHA3(password);
+        
+        let result = await login(name, encryptPass);
         if (result.logged) {
             window.location = "rooms.html"
         } else {
