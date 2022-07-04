6abbc993ccd2e3f68f22f1a4d775294097a881b7
pmbnarra <108690403+pmbnarra@users.noreply.github.com>
2022-07-05 00:32:19 +0100

    Update playersModel.js

--- models/playersModel.js
+++ models/playersModel.js
@@ -20,7 +20,7 @@ module.exports.registerUser = async function (name, password) {
       let result = await pool.query(sql1, [name]);
       
       if (result.rows.length == 1) {
-          return { status: 420, result: { msg: "Username already exists" } };
+          return { status: 200, result: { msg: "Username already exists" } };
       }else{
           let sql2 = `Insert into player(player_name, player_password) values($1,$2)`;
           await pool.query(sql2, [name,password]);
