01a2af6635a3f572476c561866e1fb8941732299
pmbnarra <108690403+pmbnarra@users.noreply.github.com>
2022-07-04 19:36:31 +0100

    Update register.js

--- public/javascripts/authentication/register.js
+++ public/javascripts/authentication/register.js
@@ -1,6 +1,9 @@
 
+
 async function registerPlayer() {
+    
     try {
+        
         let name = document.getElementById("name").value;
         let password = document.getElementById("password").value;
         
@@ -10,18 +13,28 @@ async function registerPlayer() {
         window.location = "index.html"; 
         */
         
-        var regexName = new RegExp("^(?=.{3,})");
-        var regexPass = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})");
-        
-        if (!regexName.test(name)){
+        let result = await loginName(name);  // Criei em models uma novo models de nome LoginName 
+        if (result.logged) {
+            alert("The user already exist. Choose another name");  
+        } else {
+           
+           let regexName = new RegExp("^(?=.{3,})");
+           let regexPass = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})");
+            
+           if (!regexName.test(name)){
              alert("The name is invalid. It must be longer than 3 characters");  
-        }else if (!regexPass.test(password)){
-            alert("The password is not secure. It must be longer than 8 characters, include a number, an uppercase letter and a lowercase letter");   
-        }else{
+            }else if (!regexPass.test(password)){
+                alert("The password is not secure. It must be longer than 8 characters, include a number, an uppercase letter and a lowercase letter");   
+            }else{
+                
             let res = await register(name,password);
             alert(res.msg);    
             window.location = "index.html";  
         }
+            
+        }
+        
+        
         
         
     } catch (err) {
