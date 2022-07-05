ca02a2bc5c0b885258523e4db3a428779c403473
pmbnarra <108690403+pmbnarra@users.noreply.github.com>
2022-07-05 01:04:24 +0100

    Update playersModel.js

--- models/playersModel.js
+++ models/playersModel.js
@@ -17,15 +17,14 @@ module.exports.getPlayerCards = async function (pId) {
 module.exports.registerUser = async function (name, password) {
   try {
       
-      let sql1 = `Select player_name from player where player_name = $1`;
-     
-      let result = await pool.query(sql1, [name]);
-      alert(result.rows.length);
-      if (result.rows.length == 1) {
+      let sql = `Select player_name from player where player_name = $1`;
+      let result = await pool.query(sql, [name]);
+      
+      if (result.rows.length > 0) {
           return { status: 200, result: { msg: "Username already exists" } };
       }else{
-          let sql2 = `Insert into player(player_name, player_password) values($1,$2)`;
-          await pool.query(sql2, [name,password]);
+          let sql = `Insert into player(player_name, player_password) values($1,$2)`;
+          await pool.query(sql, [name,password]);
           return { status: 200, result: { msg: "New player registered" } };
       }
   } catch (err) {
